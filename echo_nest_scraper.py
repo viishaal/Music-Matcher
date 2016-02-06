f02af5df4456d4e76057a450e2694bd2775c1ac5
vishal <vj2208@columbia.edu>
2016-02-05 23:24:14 -0500

    bug fix

--- echo_nest_scraper.py
+++ echo_nest_scraper.py
@@ -1,5 +1,6 @@
 from pyechonest import *
 import soundcloud
+import time
 
 
 BLOG_SONGS_FILE = "blog_features.txt"
@@ -8,7 +9,7 @@ BLOG_OUTPUT_FILE = "blog_vectors.txt"
 SONG_OUTPUT_FILE = "song_vectors.txt"
 
 
-config.ECHO_NEST_API_KEY="EOTNUCUCGL1YQCFLJ"
+config.ECHO_NEST_API_KEY="RA4I22UVI4B7JVCV1"
 SOUNDCLOUD_CLIENT_ID = "8cdb95c116f052404a7aabf078e48a13"
 DEFAULT_ASYNC_TIMEOUT = 30
 
@@ -25,6 +26,7 @@ def get_track_params(track_url, timeout = DEFAULT_ASYNC_TIMEOUT):
     t = track.track_from_url(url)
     song_entry = [t.id, t.danceability, t.energy, t.key, t.liveness, 
                   t.loudness, t.mode, t.speechiness, t.tempo]
+    time.sleep(10)
     return song_entry
 
 if __name__ == "__main__":
@@ -35,27 +37,36 @@ if __name__ == "__main__":
 	blog_params_map = {}
 
 	lines = f.readlines()
-	for l in lines:
+	for j,l in enumerate(lines):
+		if j == 50:
+			break;
 		splits = l.split(" ")
 		blog_id = splits[0]
 		songs = splits[-1]
 		agg_param_list = [0] * (len(splits) - 1)
 		i = 0
 		for song in songs.split("|"):
-			s_param_list = get_track_params(song)
+			try:
+				s_param_list = get_track_params(song)
+			except:
+				print "EXCEPTION ", song
+				continue
+
 			s_param_list.insert(0, song)
 			print ";".join([str(x) for x in s_param_list])
 			song_file.write(";".join([str(x) for x in s_param_list]))
+			song_file.write("\n")
 			song_file.flush()
 			agg_param_list = [sum(x) for x in zip(agg_param_list, s_param_list[2:])]
 			i = i + 1
 
 		if i != 0:
 			agg_param_list = [x*1.0/i for x in agg_param_list]
-			agg_param_list_str = ";".join([str(x for x in agg_param_list)])
-			blog_file.write(str(blog_id)+";"+agg_param_list_str)
+			agg_param_list_str = ";".join([str(x) for x in agg_param_list])
+			f_write_str = str(blog_id)+";"+str(i)+";"+agg_param_list_str
+			blog_file.write(f_write_str)
+			blog_file.write("\n")
 			blog_file.flush()
-			print str(blog_id)+";"+agg_param_list_str
 
 	blog_file.flush()
 	song_file.flush()
